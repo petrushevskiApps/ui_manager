7c1b68abf506e0282fd957721f5705a950687c67
Petrushevski Apps <petrushevskiapps@gmail.com>
2025-02-27 02:00:56 +0100

    Implemented Reactive Property

---
+++ Assets/MenuManager/Scripts/Utilitis/ReactiveProperty/IReadOnlyReactiveProperty.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MenuManager.Scripts.Utilitis
+{
+    public interface IReadOnlyReactiveProperty<T>
+    {
+        void Subscribe(Action<T> onValueChangeListener);
+        void Unsubscribe(Action<T> onValueChangeListener);
+    }
+}
