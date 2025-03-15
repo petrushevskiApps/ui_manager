dec4303b65aa32d6a0ea64c6b12834b7bdea4afa
Petrushevski Apps <petrushevskiapps@gmail.com>
2025-03-16 00:01:26 +0100

    Added Text Extension

---
+++ Assets/MenuManager/Scripts/Components/NonInteractive/Extensions/TextExtensions.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+
+namespace MenuManager.Scripts.Components.NonInteractive.Extensions
+{
+    public static class TextExtensions
+    {
+        public static void Update(this TextMeshProUGUI textMeshProUGUI, string text)
+        {
+            textMeshProUGUI.text = text;
+        }
+    }
+}
