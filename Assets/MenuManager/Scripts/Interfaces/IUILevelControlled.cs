f11daca3ce6a1a6ef76e5e4864a4473d8072ebde
Petrushevski Apps <petrushevskiapps@gmail.com>
2025-03-15 23:58:50 +0100

    Added IUILevelControlled interface

---
+++ Assets/MenuManager/Scripts/Interfaces/IUILevelControlled.cs
@@ -0,0 +1,11 @@
+﻿namespace slowBulletGames.MemoryValley
+{
+    public interface IUILevelControlled
+    {
+        void StartNextLevel();
+        void RestartLevel();
+        void ReviveAndContinueLevel();
+        void CollectReward();
+        void CollectDoubleReward();
+    }
+}
