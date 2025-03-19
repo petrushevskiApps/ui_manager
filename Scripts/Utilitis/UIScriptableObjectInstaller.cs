6754f5ce3d8ae3d435bbf1e2c5632bacd59fd5a3
Petrushevski Apps <petrushevskiapps@gmail.com>
2025-03-19 22:13:30 +0100

    Fixed Scriptable Object Installer in Submodule

---
+++ Scripts/Utilitis/UIScriptableObjectInstaller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace PetrushevskiApps.UIManager.Utilities
+{
+    [CreateAssetMenu(
+        fileName = "ScriptableObjectInstaller",
+        menuName = "Configuration/ScriptableObjectInstaller")]
+    public class MainScriptableObjectInstaller : ScriptableObjectInstaller<MainScriptableObjectInstaller>
+    {
+        [SerializeField]
+        private List<ScriptableObject> _scriptableObjectsToBind;
+
+        public override void InstallBindings()
+        {
+            foreach (ScriptableObject scriptableObject in _scriptableObjectsToBind)
+            {
+                Container
+                    .BindInterfacesTo(scriptableObject.GetType())
+                    .FromInstance(scriptableObject);
+                Container.QueueForInject(scriptableObject);
+            }
+        }
+    }
+}
