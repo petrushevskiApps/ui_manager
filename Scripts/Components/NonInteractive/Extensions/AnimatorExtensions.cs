1b48befa062d20214726e00b6d1f59185394e923
Petrushevski Apps <petrushevskiapps@gmail.com>
2025-04-18 02:56:42 +0200

    Added animation extension

---
+++ Scripts/Components/NonInteractive/Extensions/AnimatorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace MenuManager.Scripts.Components.NonInteractive.Extensions
+{
+    public static class AnimatorExtensions
+    {
+        public static void SetTriggerState(this Animator animator, string triggerState)
+        {
+            if (string.IsNullOrEmpty(triggerState))
+            {
+                animator.enabled = false;
+                return;
+            }
+            animator.enabled = true;
+            animator.SetTrigger(triggerState);
+        }
+    }
+}
