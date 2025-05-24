2a3263f39da17a34104ac65011323b9b2126c8d3
Petrushevski Apps <petrushevskiapps@gmail.com>
2025-05-24 20:06:09 +0200

    Added SFX and Delayed presentation to UI Stars

--- Scripts/Components/NonInteractive/UIStars.cs
+++ Scripts/Components/NonInteractive/UIStars.cs
@@ -1,12 +1,27 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using Plugins.UIManager.Scripts.Data;
 using UnityEngine;
+using Zenject;
 
 public class UIStars : MonoBehaviour
 {
     [SerializeField]
     private List<GameObject> _stars;
 
+    private IUiSoundSystem _uiSoundSystem;
+    private IUiAudioPalette _uiAudioPalette;
+
+    [Inject]
+    public void Setup(
+        IUiSoundSystem uiSoundSystem,
+        IUiAudioPalette uiAudioPalette)
+    {
+        _uiSoundSystem = uiSoundSystem;
+        _uiAudioPalette = uiAudioPalette;
+    }
+    
     private void Awake()
     {
         ClearStars();
@@ -19,13 +34,24 @@ public class UIStars : MonoBehaviour
 
     public void SetData(int starsCount)
     {
+        gameObject.SetActive(true);
         for (int i = 0; i < starsCount; i++)
         {
-            _stars[i].SetActive(true);
+            var index = i;
+            StartCoroutine(DelayInvoke( () =>
+            {
+                _stars[index].SetActive(true);
+                _uiSoundSystem?.PlayUiSoundEffect(_uiAudioPalette.StarShown);
+            }, 0.4f * index));
         }
-        gameObject.SetActive(true);
+
     }
 
+    private IEnumerator DelayInvoke(Action action, float delaySeconds)
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        action?.Invoke();
+    }
     private void ClearStars()
     {
         _stars.ForEach(star => star.SetActive(false));
