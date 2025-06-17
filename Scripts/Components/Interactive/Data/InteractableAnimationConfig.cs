ac764fd913851866bd82537b0c9e8e1d3b582358
Petrushevski Apps <petrushevskiapps@gmail.com>
2025-06-18 01:52:12 +0200

    Fixed animation SO class

--- Scripts/Components/Interactive/Data/InteractableAnimationConfig.cs
+++ Scripts/Components/Interactive/Data/InteractableAnimationConfig.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using JetBrains.Annotations;
+using UnityEngine;
 
 namespace TwoOneTwoGames.UIManager.Components.Interactive
 {
@@ -8,9 +9,9 @@ namespace TwoOneTwoGames.UIManager.Components.Interactive
     public class InteractableAnimationConfig : ScriptableObject
     {
         [field: SerializeField]
-        public Vector3 Scale { get; } = new(0.85f, 0.85f, 0.85f);
+        public Vector3 Scale { get; [UsedImplicitly] private set; } = new(0.85f, 0.85f, 0.85f);
 
         [field: SerializeField]
-        public float ScaleSpeed { get; } = 1f;
+        public float ScaleSpeed { get; [UsedImplicitly] private set;} = 0.5f;
     }
 }
