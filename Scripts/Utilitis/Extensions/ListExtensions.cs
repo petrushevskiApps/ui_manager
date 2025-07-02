001a038a68b5aa9e1a887fdec12273d56c2c6d9e
Petrushevski Apps <petrushevskiapps@gmail.com>
2025-07-03 00:25:39 +0200

    Implemented List Extension for Safe Get Range method

---
+++ Scripts/Utilitis/Extensions/ListExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoOneTwoGames.UIManager.Utilities.Extensions
+{
+    public static class ListExtensions
+    {
+        public static List<T> SafeGetRange<T>(this List<T> list, int index, int count)
+        {
+            if (list == null || list.Count == 0 || index >= list.Count)
+                return new List<T>();
+
+            if (index < 0)
+                index = 0;
+
+            if (count < 0)
+                count = 0;
+
+            // Calculate how many items can be returned safely
+            int safeCount = Math.Min(count, list.Count - index);
+
+            return list.GetRange(index, safeCount);
+        }
+    }
+
+}
