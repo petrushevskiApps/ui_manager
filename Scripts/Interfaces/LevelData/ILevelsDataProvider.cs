14855fd246d7a3c68f04d7f599483965942b170e
Petrushevski Apps <petrushevskiapps@gmail.com>
2025-07-03 15:20:24 +0200

    Added default values to optional paramters to the GetLevels method

--- Scripts/Interfaces/LevelData/ILevelsDataProvider.cs
+++ Scripts/Interfaces/LevelData/ILevelsDataProvider.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TwoOneTwoGames.UIManager.Interfaces
 {
     public interface ILevelsDataProvider
     {
-        List<IUILevelData> GetLevels(int page, int pageSize);
+        List<IUILevelData> GetLevels(int page = 0, int pageSize = int.MaxValue);
         IUILevelData GetLastUnlockedLevel();
     }
 }
