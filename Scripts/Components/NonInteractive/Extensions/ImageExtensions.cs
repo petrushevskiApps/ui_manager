e6ef15385b3801ef3fbdd48afc87090fd9a370b5
Petrushevski Apps <petrushevskiapps@gmail.com>
2025-07-07 15:02:55 +0200

    Implemented ImageViewData and Image Extensions

---
+++ Scripts/Components/NonInteractive/Extensions/ImageExtensions.cs
@@ -0,0 +1,34 @@
+﻿using TwoOneTwoGames.UIManager.Components.NonInteractive.NonInteractive.ViewData;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TwoOneTwoGames.UIManager.Components.NonInteractive
+{
+    public static class ImageExtensions
+    {
+        public static void SetData(this Image image, Sprite sprite)
+        {
+            if (sprite == null)
+            {
+                image.gameObject.SetActive(false);
+                return;
+            }
+
+            image.sprite = sprite;
+            image.gameObject.SetActive(true);
+        }
+        
+        public static void SetData(this Image image, ImageViewData imageViewData)
+        {
+            if (!imageViewData.IsActive)
+            {
+                image.gameObject.SetActive(false);
+                return;
+            }
+
+            image.sprite = imageViewData.Sprite;
+            image.color = imageViewData.Color;
+            image.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Scripts/Components/NonInteractive/ViewData/ImageViewData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace TwoOneTwoGames.UIManager.Components.NonInteractive.NonInteractive.ViewData
+{
+    public struct ImageViewData
+    {
+        public bool IsActive;
+        public Sprite Sprite;
+        public Color Color;
+
+        public ImageViewData(Color color, bool isActive = true, Sprite sprite = null)
+        {
+            IsActive = isActive;
+            Sprite = sprite;
+            Color = color;
+        }
+    }
+}
